126fda0758588a7235f88eb7a3e43a25c3b29629
SH <22000786@handong.edu>
2021-09-03 19:07:11 +0900

    create project

---
+++ src/com/example/lab1/Main.java
@@ -0,0 +1,21 @@
+package com.example.lab1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main lab1 = new Main();
+		lab1.a01();
+	}
+	
+	public void a01() {
+		System.out.println("Hello World!!!\n");
+		Scanner s = new Scanner(System.in);
+		System.out.print("이름 입력 : ");
+		String name = s.next();
+		System.out.println("["+name+"]님 환영합니다~");
+	}
+
+}
